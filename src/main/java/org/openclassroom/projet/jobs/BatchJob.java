50a50fd5790c708bd19ac055ea5d8bb2ea2c7431
D4RT4GNaN <maxime.blaise06@gmail.com>
2020-03-06 09:25:54 +0100

    Added logger with slf4j and log4j2 to output lines in console and in logfile

--- src/main/java/org/openclassroom/projet/jobs/BatchJob.java
+++ src/main/java/org/openclassroom/projet/jobs/BatchJob.java
@@ -6,29 +6,32 @@ import generated.libraryservice.Loan;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class BatchJob implements Job {
 
     private LibraryService libraryService = new LibraryServiceService().getLibraryServicePort();
+    private static Logger LOGGER = LoggerFactory.getLogger(BatchJob.class);
 
     @Override
     public void execute(JobExecutionContext context) {
-        System.out.println("--------------------------------------------------------------------");
-        System.out.println("Job start: " + context.getFireTime());
+        LOGGER.info("--------------------------------------------------------------------");
+        LOGGER.info("Job start: " + context.getFireTime());
         JobDetail jobDetail = context.getJobDetail();
-        System.out.println("Job name is: " + jobDetail.getJobDataMap().getString("jobName"));
+        LOGGER.info("Job name is: " + jobDetail.getJobDataMap().getString("jobName"));
 
         List<Loan> loansOverdue = libraryService.checkExpiration();
 
         for(Loan loan : loansOverdue) {
-            System.out.println(" * - A mail was sent to user " + loan.getUserID() + " for book with the reference " + loan.getBookReference());
+            LOGGER.info(" * - A mail was sent to user " + loan.getUserID() + " for book with the reference " + loan.getBookReference());
         }
 
-        System.out.println("Job end: " + context.getJobRunTime() + ", key: " + jobDetail.getKey());
-        System.out.println("Job next scheduled time: " + context.getNextFireTime());
-        System.out.println("--------------------------------------------------------------------");
+        LOGGER.info("Job end: " + context.getJobRunTime() + ", key: " + jobDetail.getKey());
+        LOGGER.info("Job next scheduled time: " + context.getNextFireTime());
+        LOGGER.info("--------------------------------------------------------------------");
     }
 
 }
